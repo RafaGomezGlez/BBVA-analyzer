f7b9df2366202a67bc48deca7f3bbfac476a5a04
Bryan González Arellano <gonzalezarellanobryan@gmail.com>
2021-10-23 00:22:05 -0500

    Update modal.js

--- src/components/Modal/modal.js
+++ src/components/Modal/modal.js
@@ -1,72 +1,55 @@
-import React, {useRef, useState} from 'react'
-import Modal from "@mui/material/Modal";
+import React, { useRef, useState } from 'react';
+import Modal from '@mui/material/Modal';
 import Tabs from '@mui/material/Tabs';
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
 import Tab from '@mui/material/Tab';
-import './modal.css'
+import './modal.css';
+import FileItem from './dropzone';
+import TabPanel, { a11yProps } from './tabs';
 
-function TabPanel(props) {
-    const { children, value, index, ...other } = props;
-  
-    return (
-      <div
-        role="tabpanel"
-        hidden={value !== index}
-        id={`simple-tabpanel-${index}`}
-        aria-labelledby={`simple-tab-${index}`}
-        {...other}
-      >
-        {value === index && (
-          <Box sx={{ p: 3 }}>
-            <Typography>{children}</Typography>
-          </Box>
-        )}
+const UploadFileModal = ({ uploadFileModal, setShowModal }) => {
+  const rootRef = useRef(null);
+  const [bank, setBank] = useState(0);
+  const [banorteFiles, setBanorteFiles] = useState([]);
+  const [bbvaFiles, setBBVAFiles] = useState([]);
+  const [santanderFiles, setSantanderFiles] = useState([]);
+
+  const handleChange = (event, newValue) => {
+    console.log(banorteFiles, bbvaFiles, santanderFiles);
+    setBank(newValue);
+  };
+
+  const handleClose = () => {
+    setShowModal(false);
+  };
+
+  return (
+    <Modal
+      disablePortal
+      disableEnforceFocus
+      disableAutoFocus
+      onClose={handleClose}
+      open={uploadFileModal}
+      closeAfterTransition
+      container={() => rootRef.current}
+    >
+      <div className='info-button'>
+        <Tabs value={bank} onChange={handleChange}>
+          <Tab label='Banorte' {...a11yProps(0)} />
+          <Tab label='Bancomer' {...a11yProps(1)} />
+          <Tab label='Santander' {...a11yProps(2)} />
+        </Tabs>
+        <TabPanel value={bank} index={0}>
+          <FileItem files={banorteFiles} setFiles={setBanorteFiles} />
+        </TabPanel>
+        <TabPanel value={bank} index={1}>
+          <FileItem files={bbvaFiles} setFiles={setBBVAFiles} />
+        </TabPanel>
+        <TabPanel value={bank} index={2}>
+          <FileItem files={santanderFiles} setFiles={setSantanderFiles} />
+        </TabPanel>
       </div>
-    );
-  }
+    </Modal>
+  );
+};
 
-function a11yProps(index) {
-    return {
-      id: `simple-tab-${index}`,
-      'aria-controls': `simple-tabpanel-${index}`,
-    };
-  }
-
-const UploadFileModal = ({uploadFileModal}) => {
-    const rootRef = useRef<HTMLDivElement>(null);
-    const [bank, setBank] = useState('Banorte')
-
-    const handleChange = () => {
-
-    }
-
-    return (
-        <Modal
-        disablePortal
-        disableEnforceFocus
-        disableAutoFocus
-        open={uploadFileModal}
-        closeAfterTransition
-        container={() => rootRef.current}>
-           <div className='info-button'>
-                <Tabs value={bank} onChange={handleChange}>
-                    <Tab label="Banorte" {...a11yProps(0)} />
-                    <Tab label="Bancomer" {...a11yProps(1)} />
-                    <Tab label="Santander" {...a11yProps(2)} />
-                </Tabs>
-                <TabPanel value={bank} index={0}>
-                    Item One
-                </TabPanel>
-                <TabPanel value={bank} index={1}>
-                    Item Two
-                </TabPanel>
-                <TabPanel value={bank} index={2}>
-                    Item Three
-                </TabPanel>
-            </div> 
-        </Modal>
-    )
-}
-
-export default UploadFileModal
+export default UploadFileModal;
