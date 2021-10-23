30d93c9fad93881e0e5b38f3afe2e749f155f94b
Fernando Cuervo <63762598+Fcuervo21@users.noreply.github.com>
2021-10-22 22:49:11 -0500

    Hola2

--- src/components/Graph/graph.js
+++ src/components/Graph/graph.js
@@ -1,54 +1,30 @@
-import React from 'react'
-import { embedDashboard } from 'amazon-quicksight-embedding-sdk';
+import React, { useEffect } from 'react';
+var QuickSightEmbedding = require("amazon-quicksight-embedding-sdk");
+// import { embedDashboard } from 'amazon-quicksight-embedding-sdk';
 
-function graph() {
+function Graph() {
+    const dashboardRef = React.createRef();
 
-    var dashboard = null;
-    function onError(payload) {
-        console.log("Do something when the dashboard fails loading");
-    };
-    function onDashboardLoad(payload) {
-        console.log("Do something when the dashboard is fully loaded.");
-    };
-
-    function embedDashboardTest() {
-        var containerDiv = document.getElementById("dashboardContainer");
-        var options = {
-            url: "https://us-east-1.quicksight.aws.amazon.com/sn/dashboards/a69101cf-58e4-4115-b17e-17432c1e7782",
-            container: containerDiv,
-            parameters: {   
-                
-            },
-            scrolling: "no",
+    const embed = () => {
+        const options = {
+            url: "https://us-east-1.quicksight.aws.amazon.com/embed/211f1e5eb2174ba1a3be692175c39ad7/dashboards/a69101cf-58e4-4115-b17e-17432c1e7782?code=AYABeGp3O8_N7Ljpnxp-cAlgBGgAAAABAAdhd3Mta21zAEthcm46YXdzOmttczp1cy1lYXN0LTE6MjU5NDgwNDYyMTMyOmtleS81NGYwMjdiYy03MDJhLTQxY2YtYmViNS0xNDViOTExNzFkYzMAuAECAQB4ZGLViwIvMWeq0eg2SdOyWJS86qkEBfgLEiY79AhlHtMBdLXJ-WY1jHC40twei4gLzQAAAH4wfAYJKoZIhvcNAQcGoG8wbQIBADBoBgkqhkiG9w0BBwEwHgYJYIZIAWUDBAEuMBEEDHwvzDltzN219Dom9QIBEIA7dzU8zc19SCtiouuPEvjLVMLE_jCkwD3iI2xgHgGm6aUIrymWHBNIT2Fim7830tcGoSelCEN89K15UEYCAAAAAAwAABAAAAAAAAAAAAAAAAAAZeQOVd_OmvE57pF7Pso2iP____8AAAABAAAAAAAAAAAAAAABAAAAm0kWHyEIKj3Xm43rXMpfuPhPE4-_N2WGvLtmaoJBEv60IHjFKuqfLCxT6msWySP0PQL9Q9f2q27opCY6Qdj-Taef7gu0d5iLvFja4YtI7yM77K6ybjuyQZKXe8VTEChWhzexibCNI8mQDic3cqF7d8JyNPXLbCeiTzp9KFS68hg1DDdgGIzy3hxRcuSmAeQ98-H9jwiCGyKzWRnEg1nRis_MsEunQiabEgc4bA%3D%3D&identityprovider=quicksight&isauthcode=true",
+            container: dashboardRef.current,
+            scrolling: "yes",
             height: "700px",
-            width: "1000px"
+            width: "1000px",
+           locale: "en-US",
+           footerPaddingEnabled: true
         };
-        dashboard = embedDashboard(options);
-        dashboard.on('error', onError);
-        dashboard.on('load', onDashboardLoad);
-    }
+       QuickSightEmbedding.embedDashboard(options)
+    };
 
-    // const showDashboard = () =>{
-    //     const containerDiv = document.getElementById("embeddingContainer");
-    //     const options = {
-    //         // replace this dummy url with the one generated via embedding API
-    //         url: "https://us-east-1.quicksight.aws.amazon.com/sn/dashboards/7b012504-97fb-461f-bb24-b4fd0202cfd6/views/95a89943-970f-47f5-a5d5-1b6da4a8aa48",  
-    //         container: containerDiv,
-    //         scrolling: "no",
-    //         height: "700px",
-    //         width: "1000px",
-    //         footerPaddingEnabled: true
-    //     };
-    //     const dashboard = embedDashboard(options);
-    //     return dashboard;
-    // };
-    
-
+    useEffect(() => {
+        embed();
+    });
     return (
-            <body onLoad={embedDashboardTest()}>
-                <div id="embeddingContainer"></div>
-            </body>
+        <div ref={dashboardRef} />
+
     )
 }
 
-export default graph
+export default Graph
